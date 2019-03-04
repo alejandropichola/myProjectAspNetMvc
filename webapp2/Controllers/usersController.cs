af320b9141aecc6c922595b31fbb9e109dc0ee2e
g4alejandro29 <g4alejandro29@gmail.com>
2019-03-04 01:49:32 -0600

    crud in asp.net

---
+++ webapp2/Controllers/usersController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using webapp2.Models;
+
+namespace webapp2.Controllers
+{
+    public class usersController : Controller
+    {
+        private sicgeEntities db = new sicgeEntities();
+        
+        // GET: users
+        public ActionResult Index()
+        {
+            var user = db.user.Include(u => u.rol);
+            return View(user.ToList());
+        }
+
+        // GET: users/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            user user = db.user.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: users/Create
+        public ActionResult Create()
+        {
+            ViewBag.RolId = new SelectList(db.rol, "id", "name");
+            return View();
+        }
+
+        // POST: users/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserId,FirstName,LastName,Email,Password,Gender,BirthDate,CreatedAt,UpdatedAt,Cui,Phone,CellPhone,RolId,Enabled")] user user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.user.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.RolId = new SelectList(db.rol, "id", "name", user.RolId);
+            return View(user);
+        }
+
+        // GET: users/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            user user = db.user.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.RolId = new SelectList(db.rol, "id", "name", user.RolId);
+            return View(user);
+        }
+
+        // POST: users/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserId,FirstName,LastName,Email,Password,Gender,BirthDate,CreatedAt,UpdatedAt,Cui,Phone,CellPhone,RolId,Enabled")] user user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.RolId = new SelectList(db.rol, "id", "name", user.RolId);
+            return View(user);
+        }
+
+        // GET: users/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            user user = db.user.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            user user = db.user.Find(id);
+            db.user.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ webapp2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(webapp2.Startup))]
+namespace webapp2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
